e4ee7f1a0e645fee88b91aee3e27fcc62785fd1b
Gurjeet <info@netqom.com>
2025-06-02 18:18:53 +0530

    Added Stripe Connect Acccount Setup

--- src/services/trade-professionals.js
+++ src/services/trade-professionals.js
@@ -1,15 +1,21 @@
 // services/trade-professionals
-import { createApiService } from "./commonService"
+import { createApiService } from "./commonService";
+import { api } from "@/utils/api";
+const TRADE_PROFESSIONALS_ENDPOINT = "/admin/trade-professional";
+const USER_TRADE_PROFESSIONALS_ENDPOINT = "/user/trade-professional";
+const USER_TRADE_PROFESSIONALS_PASSWORD_ENDPOINT = "/profile/password";
+const USER_TRADE_PROFESSIONALS_PROFILE_ENDPOINT = "/profile";
 
-const TRADE_PROFESSIONALS_ENDPOINT = "/admin/trade-professional"
-const USER_TRADE_PROFESSIONALS_ENDPOINT = "/user/trade-professional"
-const USER_TRADE_PROFESSIONALS_PASSWORD_ENDPOINT = "/profile/password"
-const USER_TRADE_PROFESSIONALS_PROFILE_ENDPOINT = "/profile"
+export const tradeProfessionalService = createApiService(TRADE_PROFESSIONALS_ENDPOINT);
 
-export const tradeProfessionalService = createApiService(TRADE_PROFESSIONALS_ENDPOINT)
+export const userTradeProfessionalService = createApiService(USER_TRADE_PROFESSIONALS_ENDPOINT, {
+    addBankAccountForTradeProfessional: async (data) => {
+        console.log('sxscsdcsd', `${USER_TRADE_PROFESSIONALS_ENDPOINT}/create-connect-account`);
+        return api.post(`${USER_TRADE_PROFESSIONALS_ENDPOINT}/create-connect-account`, data);
+    },
 
-export const userTradeProfessionalService = createApiService(USER_TRADE_PROFESSIONALS_ENDPOINT)
+});
 
-export const userTradeProfessionalPassword = createApiService(USER_TRADE_PROFESSIONALS_PASSWORD_ENDPOINT)
+export const userTradeProfessionalPassword = createApiService(USER_TRADE_PROFESSIONALS_PASSWORD_ENDPOINT);
 
-export const userTradeProfessionalProfile = createApiService(USER_TRADE_PROFESSIONALS_PROFILE_ENDPOINT)
+export const userTradeProfessionalProfile = createApiService(USER_TRADE_PROFESSIONALS_PROFILE_ENDPOINT);
--- src/views/admin/trade-professionals/view/user-right/security/index.jsx
+++ src/views/admin/trade-professionals/view/user-right/security/index.jsx
@@ -1,19 +1,23 @@
 // MUI Imports
-import Grid from '@mui/material/Grid2'
+import Grid from '@mui/material/Grid2';
 
 // Component Imports
-import ChangePassword from './ChangePassword'
-import TwoStepVerification from './TwoStepVerification'
-import RecentDevice from './RecentDevice'
+import ChangePassword from './ChangePassword';
+import AddBankAccount from './AddBankAccount';
+import TwoStepVerification from './TwoStepVerification';
+import RecentDevice from './RecentDevice';
 
-const SecurityTab = ({userId}) => {
+const SecurityTab = ({ userId }) => {
   return (
     <Grid container spacing={6}>
+      <Grid size={{ xs: 12 }}>
+        <AddBankAccount userId={userId} />
+      </Grid>
       <Grid size={{ xs: 12 }}>
         <ChangePassword userId={userId} />
       </Grid>
     </Grid>
-  )
-}
+  );
+};
 
-export default SecurityTab
+export default SecurityTab;
