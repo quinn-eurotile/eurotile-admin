c1ce983cdaad093edd1b0b69a415680bcf4eba50
Gurjeet <info@netqom.com>
2025-06-04 10:33:47 +0530

    Take a pull from nasheel code

--- src/views/admin/trade-professionals/view/user-right/security/index.jsx
+++ src/views/admin/trade-professionals/view/user-right/security/index.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 // MUI Imports
 import Grid from '@mui/material/Grid2';
 
@@ -7,8 +9,13 @@ import AddBankAccount from './AddBankAccount';
 import AdminSettings from './AdminSettings';
 import TwoStepVerification from './TwoStepVerification';
 import RecentDevice from './RecentDevice';
+import usePermission from '@/components/common/usePermission';
 
 const SecurityTab = async ({ userId }) => {
+  const canAddSupportTicket = usePermission("create-support-ticket");
+  const canUpdateSupportTicket = usePermission("update-support-ticket");
+  const canDeleteSupportTicket = usePermission("delete-support-ticket");
+  const canViewSupportTicket = usePermission("view-support-ticket");
   return (
     <Grid container spacing={6}>
       <Grid size={{ xs: 12 }}>
